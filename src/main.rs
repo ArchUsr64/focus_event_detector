d443b70c6c4f2fb7f12addf1a484e0eefddc13e2
ArchUsr64 <anshulusr@gmail.com>
2023-07-01 11:31:23 +0530

    implemented basic ansi reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{stdin, stdout, Write};
+use termion::input::MouseTerminal;
+use termion::raw::IntoRawMode;
+
+use crossterm::event::{read, DisableFocusChange, EnableFocusChange, Event, KeyCode, KeyEvent};
+use crossterm::execute;
+
+fn print_events() -> crossterm::Result<()> {
+    loop {
+        read()?;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("^[[O => Focus out");
+    println!("^[[I => Focus out");
+    println!("Press ENTER to exit safely");
+    println!("Read ANSI sequences: ");
+    execute!(stdout(), EnableFocusChange).unwrap();
+    MouseTerminal::from(stdout().into_raw_mode().unwrap())
+        .flush()
+        .unwrap();
+    std::thread::spawn(move || print_events());
+
+    stdin().read_line(&mut String::new()).unwrap();
+    execute!(stdout(), DisableFocusChange).unwrap();
 }
